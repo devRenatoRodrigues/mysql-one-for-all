9622218df3e7db8f646d5cd0df50395c01fd1cf1
devRenatoRodrigues <rehrlz@hotmail.com>
2023-06-07 16:45:26 -0300

    requisito 11 concluido

---
+++ challenges/desafio11.sql
@@ -0,0 +1,7 @@
+SELECT a.album_name AS album, COUNT(f.fav_song_id) AS favoritadas
+FROM Albums a
+INNER JOIN Songs s ON a.id = s.album_id
+LEFT JOIN Favorites f ON s.id = f.fav_song_id
+GROUP BY a.id
+ORDER BY COUNT(f.fav_song_id) DESC, a.album_name ASC
+LIMIT 3;
